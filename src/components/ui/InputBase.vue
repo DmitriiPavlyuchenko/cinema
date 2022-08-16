510521f392133e76a553a44b1af4ba523ec1cc3a
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-08-16 22:26:48 +0300

    Feature (sign-in) create default-input

---
+++ src/components/ui/InputBase.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "InputBase",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+});
+</script>
+
+<style lang="scss" module>
+input {
+  color: $black;
+}
+
+input::placeholder {
+  color: $black;
+}
+</style>
