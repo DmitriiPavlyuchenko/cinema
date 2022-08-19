26d3c18fa33096933f9e4074726afe63b4984d89
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-08-19 20:36:10 +0300

    Feature (sign-in) add localstorage helper

---
+++ src/helpers/persistanceStorage.ts
@@ -0,0 +1,16 @@
+export const getItem = (key: string) => {
+  try {
+    return JSON.parse(localStorage.getItem(key) || "{}");
+  } catch (e) {
+    console.log("Error getting daya in LS");
+    return null;
+  }
+};
+
+export const setItem = (key: string, data) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(data));
+  } catch (e) {
+    console.log("Error saving daya in LS");
+  }
+};
