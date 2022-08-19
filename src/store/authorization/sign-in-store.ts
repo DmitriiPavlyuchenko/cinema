1d7d22c2bca1e487cbb446cf49b320ad439c8c75
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-08-19 13:02:24 +0300

    Feature (sign-in) add mutations for state isAuth

--- src/store/authorization/sign-in-store.ts
+++ src/store/authorization/sign-in-store.ts
@@ -9,14 +9,24 @@ export const signInStore: Module<SignIn, RootStore> = {
     isAuth: null,
   }),
   getters: {},
+  mutations: {
+    AUTHORIZATION_SUCCESS(state) {
+      state.isAuth = true;
+    },
+    AUTHORIZATION_FAILED(state) {
+      state.isAuth = false;
+    },
+  },
   actions: {
     async authorization(context, data: Authorization) {
       try {
         const response = await signIn(data);
         if (response.status === 200) {
+          context.commit("AUTHORIZATION_SUCCESS");
           return console.log("auth");
         }
       } catch (e) {
+        context.commit("AUTHORIZATION_FAILED");
         return console.log(e);
       }
     },
