fc87f0713abdc6ef6fb5f67431fb87a10db1c98e
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-09-19 14:32:39 +0300

    Feature (transition) fix animation

--- src/components/transitions/PageTransition.vue
+++ src/components/transitions/PageTransition.vue
@@ -1,17 +1,38 @@
 <template>
-  <transition
-    enter-active-class="animate__animated animate__fadeInLeft"
-    mode="out-in"
-    name="page"
-  >
+  <transition name="page-slide">
     <slot></slot>
   </transition>
 </template>
 
 <script>
-import "animate.css";
-
 export default {
   name: "PageTransition",
 };
 </script>
+
+<style lang="scss">
+@keyframes fadeInLeft {
+  from {
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.fadeInLeft {
+  animation-name: fadeInLeft;
+}
+
+.page-slide-enter-active {
+  transform-origin: left center;
+  animation: fadeInLeft 0.8s;
+}
+
+.page-slide-leave-active {
+  transform-origin: left center;
+}
+</style>
